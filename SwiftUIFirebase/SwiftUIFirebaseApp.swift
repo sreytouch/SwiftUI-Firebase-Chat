c1b0a11511998d60161d7ccb66504720e890ae36
SreytouchLang <atouch.it@gmail.com>
2023-11-11 10:55:43 -0800

    Initial Commit

---
+++ SwiftUIFirebase/SwiftUIFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseApp.swift
+//  SwiftUIFirebase
+//
+//  Created by Sreytouch(Jessica) on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
